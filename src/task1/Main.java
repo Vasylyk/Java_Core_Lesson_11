4f01bbea4f1085bf9e01fc2f073766160692ed83
Vasylyk <nubik97777@gamil.com>
2019-11-15 13:45:42 +0200

    ArrayList

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -1,17 +1,29 @@
 package task1;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
-        Integer[]num = {3,4848,254,2164,3,17,5,9746,298,6,16};
-        Arrays.sort(num);
+        ArrayList num = new ArrayList();
+        num.add(3);
+        num.add(4848);
+        num.add(254);
+        num.add(2164);
+        num.add(3);
+        num.add(17);
+        num.add(5);
+        num.add(9746);
+        num.add(298);
+        num.add(6);
+        num.add(16);
+        Collections.sort(num);
         System.out.println("Sorted by asc");
-        System.out.println(Arrays.toString(num));
-        Arrays.sort(num, Collections.reverseOrder());
+        System.out.println(num);
+        Collections.reverse(num);
         System.out.println("Sorted by desc");
-        System.out.println(Arrays.toString(num));
+        System.out.println(num);
 
     }
 }
--- src/task2/Main.java
+++ src/task2/Main.java
@@ -1,35 +1,33 @@
 package task2;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Random r = new Random();
-        Car [][] carArr = new Car[r.nextInt(5)+1][r.nextInt(5)+1];
-        for (int i = 0; i <carArr.length ; i++) {
-            for (int j = 0; j <carArr[i].length ; j++) {
-                Car car = new Car(r.nextInt(120)+20,r.nextInt(39)+1980,
-                        new Wheel(r.nextInt(30)+20, "leather"),new Engine(r.nextInt(9)+2));
-                carArr[i][j]=car;
-            }
+        ArrayList carArr = new ArrayList();
+        for (int i = 0; i <r.nextInt(10)+1; i++) {
+            Car car = new Car(r.nextInt(120)+20,r.nextInt(39)+1980,
+                    new Wheel(r.nextInt(30)+20, "leather"),new Engine(r.nextInt(9)+2));
+            carArr.add(car);
+
         }
         System.out.println("1-toString, 2-fillTheSameCar");
         Scanner sc = new Scanner(System.in);
         switch (sc.nextInt()){
             case 1:{
-                System.out.println(Arrays.deepToString(carArr));
+                System.out.println(carArr);
                 break;
             }
             case 2:{
                 Car c = new Car(100,2000,new Wheel(30,"leather"),new Engine(6));
-                for (int i = 0; i <carArr.length ; i++) {
-                    for (int j = 0; j <carArr[i].length; j++) {
-                        Arrays.fill(carArr[i], c);
-                    }
+                carArr.clear();
+                for (int i = 0; i <r.nextInt(10)+1; i++) {
+                    carArr.add(c);
                 }
-                System.out.println(Arrays.deepToString(carArr));
+                System.out.println(carArr);
                 break;
             }
         }
